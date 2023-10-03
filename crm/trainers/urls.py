ec4d096147471b81f33676a5397c738bf3eb0daf
LosPiesDescalzos <katyaj2001@mail.ru>
2023-10-03 23:59:04 +0300

    add delete functionality to director page

--- crm/trainers/urls.py
+++ crm/trainers/urls.py
@@ -8,16 +8,29 @@ urlpatterns = [
     path('logout', views.log_out, name='logout'),
     path('main', views.main, name='main'),
     path('main/mark', views.mark, name='mark'),
+
     path('main/area_creation', views.area_creation, name='area_creation'),
+    path('main/area_delete', views.area_delete, name='area_delete'),
+
     path('main/role_creation', views.role_creation, name='role_creation'),
+    path('main/role_delete', views.role_delete, name='role_delete'),
+
     path('main/trainer_state_creation', views.trainer_state_creation, name='trainer_state_creation'),
+    path('main/trainer_state_delete', views.trainer_state_delete, name='trainer_state_delete'),
+
     path('main/client_state_creation', views.client_state_creation, name='client_state_creation'),
+    path('main/client_state_delete', views.client_state_delete, name='client_state_delete'),
+
     path('main/abonement_creation', views.abonement_creation, name='abonement_creation'),
+    path('main/abonement_delete', views.abonement_delete, name='abonement_delete'),
+
     path('main/sport_type_creation', views.sport_type_creation, name='sport_type_creation'),
+    path('main/sport_type_delete', views.sport_type_delete, name='sport_type_delete'),
+
+    path('clients/team_creation', views.team_creation, name='team_creation'),
     path('clients', views.clients, name='clients'),
     path('client/<int:client_id>', views.client_info, name='client_info'),
     path('client/add/action', views.client_add_action, name='client_add_action'),
-    path('clients/team_creation', views.team_creation, name='team_creation'),
     path('trainers', views.trainers, name='trainers'),
     path('trainers/add_action', views.trainers_add_action, name='trainers_add_action'),
     path('schedule', views.schedule, name='schedule')
