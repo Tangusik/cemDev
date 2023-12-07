ee537668669920fda03a64710671ce83592d2d26
LosPiesDescalzos <katyaj2001@mail.ru>
2023-12-07 13:15:22 +0300

    add some front

--- crm/frontend/src/components/Footer/index.js
+++ crm/frontend/src/components/Footer/index.js
@@ -3,15 +3,15 @@ import styles from './index.module.css';
 const Footer = () => {
     return (
         <footer id="footer">
-        <div className={styles.nav_footer}>
+        <div className={styles.navFooter}>
             <div className={styles.footerCol}>
                 <h4>О нас</h4>
             </div>
             <div className={styles.footerCol}>
                 <h4>Документация</h4>
                 <div className={styles.socialLinks}>
-                    <a href="https://vk.com/pies_descalzos">ВК</a>
-                    <a href="https://github.com/LosPiesDescalzos">Git</a>
+                    <a className={styles.link} href="https://vk.com/pies_descalzos">ВК</a>
+                    <a className={styles.link} href="https://github.com/LosPiesDescalzos">Git</a>
                 </div>
             </div>
             <div className={styles.footerCol}>
